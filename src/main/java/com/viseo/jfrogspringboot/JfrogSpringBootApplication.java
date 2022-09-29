8f1e76529f3498871c097a05347efa7fcec896eb
Jayden <jaydennleemc@gmail.com>
2022-09-29 22:53:16 +0800

    init project

---
+++ src/main/java/com/viseo/jfrogspringboot/JfrogSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.viseo.jfrogspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfrogSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfrogSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viseo/jfrogspringboot/JfrogSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viseo.jfrogspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfrogSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
